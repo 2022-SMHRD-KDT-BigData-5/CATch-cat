cb00428db893da3bbf04f0171ba5640128b7492f
smhrd <smhrd@211.228.36.206>
2022-07-19 11:50:01 +0900

    희연 파일업로드

---
+++ CATch/src/main/java/com/smhrd/web/UploadController.java
@@ -0,0 +1,17 @@
+package com.smhrd.web;
+
+
+import javax.annotation.Resource;
+
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UploadController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+}
