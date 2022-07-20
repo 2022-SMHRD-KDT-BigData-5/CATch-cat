fb830517650c92f1a1746d58d8c102c0c9abee18
smhrd <smhrd@host.docker.internal>
2022-07-20 17:04:13 +0900

    희연 - 고양터

--- CATch/src/main/java/com/smhrd/web/CareController.java
+++ CATch/src/main/java/com/smhrd/web/CareController.java
@@ -1,15 +1,14 @@
 package com.smhrd.web;
 
 
-
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -30,7 +29,6 @@ public class CareController {
 	@RequestMapping("/caremap.do")
 	public String carelist(Model model,HttpServletRequest request, CareBoard careBoard)  {
 		
-		System.out.println(careBoard.getCare_name());
 		
 		return "map";
 		
@@ -39,6 +37,16 @@ public class CareController {
 	@RequestMapping("/carezone.do")
 	public String carezone() {
 		return "carezone";
+	}
+	
+	@RequestMapping("/insertAddress.do")
+	public String insertAddress(CareBoard careBoard, Model model) {
+		int cnt = 0;
+		cnt = mapper.insertAddress(careBoard);
+		if(cnt>0) {
+			System.out.println("db에 급식소 등록 성공");
+		}
+		return "redirect:/carezone.do";
 	}
 
 	
